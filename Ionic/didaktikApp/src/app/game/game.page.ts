97ab55babb0629a83065be49ba768b634052f074
JosebaHL09 <71440171+JosebaHL09@users.noreply.github.com>
2022-01-11 14:08:39 +0100

    mapa con estilo selecto

--- Ionic/didaktikApp/src/app/game/game.page.ts
+++ Ionic/didaktikApp/src/app/game/game.page.ts
@@ -16,18 +16,60 @@ export class GamePage implements OnInit {
   }
 
   loadMap() {
+    let style = [
+      {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      }
+    ]
     // create a new map by passing HTMLElement
     const mapEle: HTMLElement = document.getElementById('map');
     // create LatLng object
-    const myLatLng = {lat: 4.658383846282959, lng: -74.09394073486328};
+    
+    const myLatLng = { lat: 43.1677686, lng: -2.6411662 };
     // create map
     this.map = new google.maps.Map(mapEle, {
-      center: myLatLng, 
-      zoom: 12
+      center: myLatLng,
+      zoom: 15,
+      styles: style,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      streetViewControl: false,
+      disableDefaultUI: true
     });
-  
+
     google.maps.event.addListenerOnce(this.map, 'idle', () => {
       mapEle.classList.add('show-map');
+
     });
   }
 
