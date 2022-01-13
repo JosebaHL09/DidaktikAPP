fd155aa1c0af4fed32655ebbef652c0c43f36251
JosebaHL09 <71440171+JosebaHL09@users.noreply.github.com>
2022-01-13 10:25:46 +0100

    punto de posicion con rango jejeje siuuuuuuuuuuuuuuuu

--- Ionic/didaktikApp/src/app/game/game.page.ts
+++ Ionic/didaktikApp/src/app/game/game.page.ts
@@ -22,7 +22,6 @@ export class GamePage implements OnInit {
   }
 
   loadMap() {
-
     this.geolocation.getCurrentPosition({
       timeout: 10000,
       enableHighAccuracy: true
@@ -68,26 +67,55 @@ export class GamePage implements OnInit {
       // create a new map by passing HTMLElement
       const mapEle: HTMLElement = document.getElementById('map');
       // create LatLng object
-
       const myLatLng = new google.maps.LatLng(this.lati, this.long);
+      
       // create map
       this.map = new google.maps.Map(mapEle, {
         center: myLatLng,
-        zoom: 15,
+        zoom: 16,
         styles: style,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         streetViewControl: false,
         disableDefaultUI: true
+
       });
+
+
 
       google.maps.event.addListenerOnce(this.map, 'idle', () => {
         mapEle.classList.add('show-map');
 
       });
 
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          scale: 10,
+          fillOpacity: 1,
+          strokeWeight: 2,
+          fillColor: '#5384ED',
+          strokeColor: '#ffffff',
+        }
+      });
+      var circle = new google.maps.Circle({
+        center:marker.getPosition(), 
+        radius: 100, 
+        fillColor: "#0000FF", 
+        fillOpacity: 0.1, 
+        map: this.map,
+        strokeColor: "#FFFFFF", 
+        strokeOpacity: 0.1, 
+        strokeWeight: 2 
+        });
+
+      marker.setMap(this.map);
+      //circle.setMap(this.map);
+
     }).catch((error) => {
       console.log('Error getting location', error);
     });
+    
 
   }
 
