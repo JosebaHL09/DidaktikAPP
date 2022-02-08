faca729e581fca63498d0638c4116b00a168fe71
JosebaHL09 <71440171+JosebaHL09@users.noreply.github.com>
2022-02-08 13:37:08 +0100

    boton exit

--- Ionic/didaktikApp/src/app/app.component.ts
+++ Ionic/didaktikApp/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CachingService } from './services/caching.service';
 import { Storage } from '@capacitor/storage';
+import { Platform } from '@ionic/angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -9,27 +11,29 @@ import { Storage } from '@capacitor/storage';
 export class AppComponent {
 
   userName2: String;
-
-  constructor(private cachingService: CachingService) { this.cachingService.initStorage(); }
+  platform
+  constructor(private cachingService: CachingService, platform: Platform) {
+    this.cachingService.initStorage();
+    this.platform = platform
+  }
   ngOnInit() {
     this.checkName();
   }
   async checkName() {
     this.userName2 = await Storage.get({ key: 'name' }).then();
     this.userName2 = JSON.stringify(this.userName2);
-    this.userName2 =  this.userName2.substring(10,this.userName2.length - 2);
+    this.userName2 = this.userName2.substring(10, this.userName2.length - 2);
 
   };
   public appPages = [
     { title: 'Inicio', url: '/home', icon: 'home' },
     { title: 'Instrucciones', url: '/instrucciones', icon: 'information-circle' },
     { title: 'Creditos', url: '/creditos', icon: 'people' },
-    { title: 'Castillo', url: '/castillo-info', icon: 'heart' },
-    { title: 'Mural', url: '/mural-info', icon: 'archive' },
-    { title: 'Estatua', url: '/estatua-info', icon: 'trash' },
-    { title: 'Juego-castillo', url: '/castillo-juego', icon: 'game-controller' },
-    { title: 'Juego-puerto', url: '/puerto-juego', icon: 'game-controller' },
+  
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
+  exitApp() {
+    this.platform.exitApp();
+  }
 }
